2e44f61cdfb2706c3951c7ac462e7002c57af694
Harrison Ifeanyichukwu <Harrisonifeanyichukwu@gmail.com>
2019-10-11 10:34:04 +0200

    fix: catch error while decoding values

--- packages/gatsby-source-prismic-graphql/src/utils/parseQueryString.ts
+++ packages/gatsby-source-prismic-graphql/src/utils/parseQueryString.ts
@@ -1,7 +1,13 @@
 export function parseQueryString(qs: string = '', delimiter: string = '&'): Map<string, string> {
   return new Map(
     qs.split(delimiter).map(item => {
-      const [key, ...value] = item.split('=').map(part => decodeURIComponent(part.trim()));
+      const [key, ...value] = item.split('=').map(part => {
+        try {
+          return decodeURIComponent(part.trim());
+        } catch (ex) {
+          return part.trim();
+        }
+      });
       return [key, value.join('=')] as [string, string];
     })
   );
